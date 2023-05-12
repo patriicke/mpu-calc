0da2dd9cd4410347c04d9f1f9839755f1639afc1
Patrick NDAYAMBAJE <97700182+patriicke@users.noreply.github.com>
2023-05-12 06:52:56 +0200

    update(): package

--- dist/calculator.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const calculator: {
-    add: (n1: number, n2: number) => number;
-    multiply: (n1: number, n2: number) => number;
-    divide: (n1: number, n2: number) => number;
-    subtract: (n1: number, n2: number) => number;
-};
--- dist/calculator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculator = void 0;
-const add = (n1, n2) => n1 + n2 + 10;
-const subtract = (n1, n2) => n1 - n2;
-const multiply = (n1, n2) => n1 * n2;
-const divide = (n1, n2) => n1 / n2;
-exports.calculator = { add, multiply, divide, subtract };
